d3f3f87a6024516d4a75adc4084ac36b3eee2d12
alexmonjaraz-at-890488113368 <monjaraz.alexandro@gmail.com>
2020-03-09 11:46:37 -0500

    added config files for spring security. there is one example user for now.

---
+++ src/main/java/com/alexmonjaraz/workoutapp/config/SecurityWebAppInitializer.java
@@ -0,0 +1,10 @@
+package com.alexmonjaraz.workoutapp.config;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class SecurityWebAppInitializer  extends AbstractSecurityWebApplicationInitializer {
+
+
+}
