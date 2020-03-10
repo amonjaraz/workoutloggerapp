444c6b0da7adf2f297736862c212ecc54fbcccab
alexmonjaraz-at-890488113368 <monjaraz.alexandro@gmail.com>
2020-03-10 11:47:15 -0500

    updated security to use database instead of in memory users

--- src/main/java/com/alexmonjaraz/workoutapp/config/SecurityConfig.java
+++ src/main/java/com/alexmonjaraz/workoutapp/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.alexmonjaraz.workoutapp.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,11 +13,17 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource myDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication().withUser(users.username("alex").password("pass123").roles("ADMIN"));
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//		
+//		auth.inMemoryAuthentication().withUser(users.username("alex").password("pass123").roles("ADMIN"));
+//	
+		auth.jdbcAuthentication().dataSource(myDataSource);
 	}
 }
